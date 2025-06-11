e67bcef71f977b9889cb663576b3a535bd90aed8
Bharatkumar17 <70887566+Bharatkumar17@users.noreply.github.com>
2025-06-11 19:18:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,20 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = "7982396596:AAGZJZj1Gqc6XV46-9nXl-af1mhwAnLV6PU"
-    API_ID = 28094744
-    API_HASH = "a75af4285edc7747c57bb19147ca0b9b"
-    ADMIN = '5680454765'.split(',')
+    BOT_TOKEN = "7278400549:AAEV4sUsAg1Mz_5m2P5mlB069MG2Y4HFNCA"
+    API_ID = 26729193
+    API_HASH = "a94598ef642481e35466292df95f251e"
+    ADMIN = '1012164907'.split(',')
     ADMIN_ID = [int(id) for id in ADMIN]
-    DB_URL = "mongodb+srv://tmglcd:kI1UijMr2jJXyOXY@cluster0.unjzi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    DB_NAME = "WARRIOR_EXTRACTOR"
-    TXT_LOG = -10023125
-    AUTH_LOG = -10023802
-    HIT_LOG = -100247596
-    DRM_DUMP = -10024562
-    CHANNEL = -10022752
-    CH_URL = "https://t.me/+vX1JFN-Mzhl"
-    OWNER = "https://t.me/mrcraacker"
+    DB_URL = "mongodb+srv://Drmbaby:drmbaby@cluster0.xwlbw7t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    DB_NAME = "Drmbaby"
+    TXT_LOG = -1002607015025
+    AUTH_LOG = -1002607015025
+    HIT_LOG = -1002607015025
+    DRM_DUMP = -1002607015025
+    CHANNEL = -1002607015025
+    CH_URL = "https://t.me/rajsthangkguru1"
+    OWNER = "https://t.me/rajsthangkguru1"
     THUMB_URL = "https://vault.pictures/media/images/b9/ad/12/b9ad123d95cd49465fcb954d.jpg" #Replace by with your Thumb URL
     HOST = "https://drm-api-five.vercel.app"
 
